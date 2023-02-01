8dc500c7d3ef0d6cac37b7f0714ec621e72fdc78
Ding Shuai <1203823603@qq.com>
2023-02-01 18:54:03 +0800

    线程共享

--- thread.h
+++ thread.h
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
+int value;
 
-void *threadFunc(void *arg) {
-    printf("In New thread\n");
+void *hello(void *arg) {
+    for (int i = 0; i < 3; ++i) {
+        printf("hello(%d)\n", value++);
+    }
+}
+
+void *world(void *arg) {
+    for (int i = 0; i < 3; ++i) {
+        printf("world(%d)\n", value++);
+    }
 }
 
 int thread() {
-    pthread_t tid;
+    srand(time(NULL));
+    pthread_t tid,tid2;
 
-    pthread_create(&tid, NULL, threadFunc, NULL);
+    pthread_create(&tid, NULL, hello, NULL);
+    pthread_create(&tid2, NULL, world, NULL);
 
     pthread_join(tid, NULL);
+    pthread_join(tid2, NULL);
 
-    printf("In main thread \n");
+    printf("In main thread(%d) \n",value);
 
     return 0;
 
